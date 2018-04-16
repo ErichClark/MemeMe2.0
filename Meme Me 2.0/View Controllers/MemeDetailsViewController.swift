8f64b56acc9b9fc23a6b3a6e49b13aba17e6daef
ErichClark <erich.clark@gmail.com>
2018-04-15 23:25:07 -0400

    Added MemeDetailsViewController

---
+++ Meme Me 2.0/View Controllers/MemeDetailsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MemeDetailsViewController.swift
+//  Meme Me 2.0
+//
+//  Created by Erich Clark on 4/15/18.
+//  Copyright © 2018 Erich Clark. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailsViewController: UIViewController {
+
+    var meme: Meme!
+    
+    @IBOutlet weak var sentMemeDetailView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
